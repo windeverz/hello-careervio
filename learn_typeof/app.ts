d9001856903405eeee2542e3e4b8575b8267f45c
windeverz <bds_si@hotmail.com>
2022-04-10 23:18:11 +0700

    learn_typeof

---
+++ learn_typeof/app.ts
@@ -0,0 +1,13 @@
+// Learn Type Of
+let a = false;
+let b = "ABC";
+let c = 113113;
+let d = null;
+let e = "";
+// let f = 13qed; 
+
+console.log(typeof a);  // boolean
+console.log(typeof b);  // string
+console.log(typeof c);  // number
+console.log(typeof d);  // object
+console.log(typeof e);  // string
---
+++ learn_typeof/out/app.js
@@ -0,0 +1,13 @@
+// Learn Type Of
+var a = false;
+var b = "ABC";
+var c = 113113;
+var d = null;
+var e = "";
+// let f = 13qed; 
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+//# sourceMappingURL=app.js.map
