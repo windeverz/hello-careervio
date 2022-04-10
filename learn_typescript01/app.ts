062ed64d80e848247f7ac314eb8c7022b59a2be5
windeverz <bds_si@hotmail.com>
2022-04-10 19:46:23 +0700

    learn string in TS

--- learn_typescript01/app.ts
+++ learn_typescript01/app.ts
@@ -1,3 +1,25 @@
 let text:string;
 text = "Hello World";
 console.log(text);
+
+// 1. define a variable
+let str:string = "Everything can work";
+
+// 2. find length of string
+console.log(str.length);
+
+// 3. use charAt() to find specific character
+console.log(str.charAt(9));
+
+// 4. use indexOf() to find specific character
+console.log(str[9]);
+
+// 5. two ways different to find specific character
+console.log(str.charAt(30));
+console.log(str[30]);   // undefined
+
+// 6. concat two strings
+let firstWord:string = "Hello";
+let secondWord:string = "World";
+let concatWord:string = `${firstWord} ${secondWord}`;
+console.log(concatWord);
--- learn_typescript01/out/app.js
+++ learn_typescript01/out/app.js
@@ -1,4 +1,20 @@
 var text;
 text = "Hello World";
 console.log(text);
+// 1. define a variable
+var str = "Everything can work";
+// 2. find length of string
+console.log(str.length);
+// 3. use charAt() to find specific character
+console.log(str.charAt(9));
+// 4. use indexOf() to find specific character
+console.log(str[9]);
+// 5. two ways different to find specific character
+console.log(str.charAt(30));
+console.log(str[30]); // undefined
+// 6. concat two strings
+var firstWord = "Hello";
+var secondWord = "World";
+var concatWord = "".concat(firstWord, " ").concat(secondWord);
+console.log(concatWord);
 //# sourceMappingURL=app.js.map
