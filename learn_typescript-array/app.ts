79d4a32212789ee78c14480624b3d6391db20493
windeverz <bds_si@hotmail.com>
2022-04-11 21:50:17 +0700

    learn-array

---
+++ learn_typescript-array/app.ts
@@ -0,0 +1,8 @@
+// Learn Array
+// 1. ผลลัพธ์ของความยาว array คืออะไร
+
+let fruits = ["Apple", "Pear", "Orange"];
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+console.log(fruits.length); // 4
---
+++ learn_typescript-array/out/app.js
@@ -0,0 +1,7 @@
+// Learn Array
+// 1. ผลลัพธ์ของความยาว array คืออะไร
+var fruits = ["Apple", "Pear", "Orange"];
+var shoppingCart = fruits;
+shoppingCart.push("Banana");
+console.log(fruits.length); // 4
+//# sourceMappingURL=app.js.map
